d651c0bd4a2bf85e392a2c8c6580b0160fdc0d47
Caleb Durojaiye <dco2.caleb@gmail.com>
2022-11-10 22:36:11 +0100

    Refactored BOokItem

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -6,17 +6,24 @@ import React from 'react';
 
 const BookItem = ({thumbnail, title, misc}) => {
 
+  const dropdownMenu = (
+    <div>
+      <ol>
+        <button onclick="dosomething">Currently Reading</button>
+        <button onclick="dosomething">Want to Read</button>
+        <button onclick="dosomething">Read</button>
+      </ol>
+    </div>
+    );
+  
+  const displaydropdownMenu = false;
+
   return(
     <div>
       <img src={thumbnail}></img>
       <p>{title}</p>
-      <select>
-        <option value="move" disabled>Move to...</option>
-        <option value="currentlyReading">Currently Reading</option>
-        <option value="wantToRead">Want to Read</option>
-        <option value="read">Read</option>
-        <option value="none">None</option>
-      </select>
+      <button onclick="changedisplaydropdownMenutoTrue"></button>
+      {displaydropdownMenu && dropdownMenu}
     </div>
   )
 }
