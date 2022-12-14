abfcc5e05c954bac5962e9090337146182007329
Caleb Durojaiye <dco2.caleb@gmail.com>
2022-12-14 16:30:27 +0100

    Modified & Removed unnecessary code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,13 @@
 import React, { useEffect } from "react";
 import BookShelf from "../BookShelf";
-import {Link} from "react-router-dom";
-
-//import Navbar from "../Navbar";
 
 const Home = () => {
 
   return(
     <div>
-      <Link to="/search">Search</Link>
       <BookShelf shelf={"reading"} />
       <BookShelf shelf={"to-be-read"} />
       <BookShelf shelf={"read"} />
-      {/* <BookShelf shelf={"readd"} /> */}
     </div>
   );
 }
--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -1,15 +1,12 @@
 import React, {useEffect} from 'react';
 import { useBookItemContext } from "../BookItemContext";
 import BookItem from "../BookItem";
-import {Link} from "react-router-dom";
-
-import SearchBar from '../SearchBar';
-import SearchPageResults from '../SearchPageResults';
 
 const Search = () => {
 
   const {apiLibrary, getQueriedApiLibrary, loading} = useBookItemContext();
   let BookList = <div></div>;
+  let status = <></>;
 
   useEffect(() => {
     getQueriedApiLibrary();
@@ -17,24 +14,22 @@ const Search = () => {
   }, []);
 
   if(loading){
-    return <p>From Search Loading...</p>;
+    status = <p style={{margin: "1em"}}>Loading...</p>;
   }else{
     BookList = apiLibrary.map((book) =>
-    <BookItem key={book.id} book={book} />
+    <div>
+    <BookItem key={book.id} book={book} page={"SEARCH_PAGE"} />
+    </div>
     );
   }
 
   return(
     <>
-    <Link to="/">Home</Link>
+    {status}
     <div className='search-result'>
       {BookList}
     </div>
     </>
-    // <div>
-    //   <SearchBar/>
-    //   <SearchPageResults/>
-    // </div>
   )
 }
 
