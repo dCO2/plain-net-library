df18efad92c4240d6a1fcc859138d20d7e2c0473
Caleb Durojaiye <dco2.caleb@gmail.com>
2022-12-14 16:32:30 +0100

    Modified Bookshelf to include headings

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -5,27 +5,34 @@ import BookItem from "./BookItem";
 const BookShelf = ({shelf}) => {
 
   const {state, loading, getQueriedApiLibrary} = useBookItemContext();
-  const readingHeader = <h3><i>Reading</i> SHELF</h3>;
-  const readHeader = <h3><i>Read</i> SHELF</h3>;
-  const tobereadHeader = <h3><i>To Be Read</i> SHELF</h3>;
+  const readingHeader = <h2 style={{fontWeight:"normal"}}> Reading SHELF</h2>;
+  const readHeader = <h2 style={{fontWeight:"normal"}}>Read SHELF</h2>;
+  const tobereadHeader = <h2 style={{fontWeight:"normal"}}>To Be Read SHELF</h2>;
+
+  let bookshelfStatus = <></>;
 
   let BookList = <div></div>;
 
   if(loading){
-    return <p>Loading...</p>;
+    bookshelfStatus =  <p>Loading...</p>;
   }else{
     BookList = state.map((book) =>
-    book.shelf===shelf? <BookItem key={book.id} book={book} /> :<></>
+    book.shelf===shelf? <div><BookItem key={book.id} book={book} page={"HOME_PAGE"}/></div> :<></>
     );
   }
 
   return(
-    <>
-    {shelf==='reading'?readingHeader:<></>}
-    {shelf==='to-be-read'?tobereadHeader:<></>}
-    {shelf==='read'?readHeader:<></>}
-    {BookList}
-    </>
+    <div className="shelf-container">
+      <div className='shelf-header'>
+        {bookshelfStatus}
+        {shelf==='reading'?readingHeader:<></>}
+        {shelf==='to-be-read'?tobereadHeader:<></>}
+        {shelf==='read'?readHeader:<></>}
+      </div>
+      <div className='search-result'>
+        {BookList}
+      </div>
+    </div>
   );
 }
 
