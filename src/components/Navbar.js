b40fa67f24c1d6a7482c79d3d53adca70818400a
Caleb Durojaiye <dco2.caleb@gmail.com>
2022-12-14 16:33:55 +0100

    properly implemented navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,23 @@
-
+import {Link} from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
   return (
-    <h1>Primordial Library</h1>
+    <div className="navbar">
+      <div className="home-page">
+      <h1>
+        <Link to="/"><i className="fa-solid fa-book"></i></Link>
+        <span className="navbar-title"> Plain Net Library</span>
+        <span className="alt-navbar-title"> PNL</span>
+      </h1>
+      </div>
+      <SearchBar/>
+      <div className="search-page">
+      <h1>
+        <Link style={{float:"right", textDecoration:"underline"}} to="/search"><i className="fa-solid fa-magnifying-glass"></i></Link>
+      </h1>
+      </div>
+    </div>
   ); 
 };
 
