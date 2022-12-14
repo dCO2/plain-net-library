2cea8a580d8c9faee20f61bd3e5ddacf3fee4fd0
Caleb Durojaiye <dco2.caleb@gmail.com>
2022-12-14 16:19:25 +0100

    Modified BookItem Card for Home & Search page

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,23 +1,50 @@
 import React from 'react';
 import { useBookItemContext } from './BookItemContext';
 
-const BookItem = ({book}) => {
+const BookItem = ({book, page}) => {
   const {id, volumeInfo} = book;
   const {title, authors, imageLinks} = volumeInfo;
   const {thumbnail} = imageLinks;
-  const {addBookToUserLibrary} = useBookItemContext();
+  const {addBookToUserLibrary, moveBookInUserLibrary, deleteBookFromUserLibrary} = useBookItemContext();
+  let actionMenu = null;
 
-  // const dropdownMenu = (
-  //   <div>
-  //     <ol>
-  //       <button onclick="dosomething">Currently Reading</button>
-  //       <button onclick="dosomething">Want to Read</button>
-  //       <button onclick="dosomething">Read</button>
-  //     </ol>
-  //   </div>
-  //   );
-  
-  // const displaydropdownMenu = false;
+  if(page === "SEARCH_PAGE"){
+    actionMenu = (
+    <div className='card-right-dropdown-content'>
+      <button onClick={() => addBookToUserLibrary(book, "read")} className="button">
+      <span style={{fontSize: "0.8em"}}>Add To Read</span>
+      </button>
+      <button onClick={() => addBookToUserLibrary(book, "to-be-read")} className="button">
+        <span style={{fontSize: "0.8em"}}>Add To To-Be-Read</span>
+      </button>
+      <button onClick={() => addBookToUserLibrary(book, "reading")} className="button">
+        <span style={{fontSize: "0.8em"}}>
+        Add To Reading
+        </span>
+      </button>
+    </div>
+    );
+  }else if(page === "HOME_PAGE"){
+    actionMenu = (
+    <div className='card-right-dropdown-content'>
+      <button onClick={() => moveBookInUserLibrary(book, "read")} className="button">
+      <span style={{fontSize: "0.8em"}}>Move To Read</span>
+      </button>
+      <button onClick={() => moveBookInUserLibrary(book, "to-be-read")} className="button">
+        <span style={{fontSize: "0.8em"}}>Move To To-Be-Read</span>
+      </button>
+      <button onClick={() => moveBookInUserLibrary(book, "reading")} className="button">
+        <span style={{fontSize: "0.8em"}}>
+        Move To Reading
+        </span>
+      </button>
+      <button onClick={() => deleteBookFromUserLibrary(book)} className="button">
+        <span style={{fontSize: "0.8em"}}>Delete</span>
+      </button>
+    </div>
+    );
+  }
+
 
   return(
     <div className='card bg-light'>
@@ -25,13 +52,16 @@ const BookItem = ({book}) => {
         <img src={thumbnail} alt={id} style={{maxHeight: "200px"}}></img>
       </div>
       <div className='card-right'>
-        <ul>
-          <li><span style={{fontSize: "0.8em"}}>Title: <span style={{fontSize: "1.5em"}}><b><i>{(title.length > 80)? title.substr(0, 80) :title}</i></b></span></span></li>
-          <li><span style={{fontSize: "0.8em"}}>Author: <b>{authors?authors[0]:""}</b></span></li>
-          <li><button onClick={() => addBookToUserLibrary(book, "read")}>Add Book To Read</button></li>
-          <li><button onClick={() => addBookToUserLibrary(book, "to-be-read")}>Add Book To To-Be-Read</button></li>
-          <li><button onClick={() => addBookToUserLibrary(book, "reading")}>Add Book To Reading</button></li>
-        </ul>
+            <div className='card-right-title'>
+              {(title.length > 80)? title.substr(0, 80) :title}
+            </div>
+            <div className='card-right-author'>
+              <span>&emsp;</span><b>{authors?(authors[0].length > 59? authors[0].substr(0,59) :authors[0]):null}</b>
+            </div>
+            <div className='card-right-button'>
+              <span style={{fontSize: "0.8em", textDecoration: "underline", textDecorationThickness: "0.2em", textDecorationColor: "black" }}>&emsp;<i>action menu</i></span>
+              {actionMenu}
+            </div>
       </div>
     </div>
   )
